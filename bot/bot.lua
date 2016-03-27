744d1772666060302aeb39a3e863bbc4f80024f9
aminsa2000 <sabeti59@yahoo.com>
2016-03-27 13:34:41 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {41471906},
     admin_users = {},
     disabled_channels = {}
   }
